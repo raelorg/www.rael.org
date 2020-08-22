cdfac893256319265675c33275132e9eb37b18e3
raelorg <69607164+raelorg@users.noreply.github.com>
2020-08-22 15:44:55 -0400

    Add files via upload

--- hello-theme-child-master/lib/elohimnet.php
+++ hello-theme-child-master/lib/elohimnet.php
@@ -918,10 +918,6 @@ function custom_validation_7( $validation_result ) {
 	$rest = substr($lastname, -2);    		  // VF
 	$length_rest = strlen( $rest );			  // 2
 	$domain = strstr($email, '@');            // @clip-share.net
-	$pos_firstname = strpos( $lastname, $firstname );   // false ou numérique
-	if ( !is_numeric( $pos_firstname ) ) {
-		$pos_firstname = 666;
-	}
 
 	$row = checkSpam( $email );
 
@@ -933,7 +929,7 @@ function custom_validation_7( $validation_result ) {
 		 || ( $domain === '@clip-share.net' )
 		 ||	(   ( is_numeric( $firstname )  ) 
 			 && ( is_numeric( $lastname )  ) ) 
-		 || (   ( $pos_firstname === 0 ) 
+		 || (   ( strpos( $lastname, $firstname ) === 0 )  // pos start at 0
 			 && ( ( $length_lastname - $length_firsname) === 2 )
 			 && ( ctype_upper($rest) )
 			 && ( $length_rest === 2 ) ) ) {
