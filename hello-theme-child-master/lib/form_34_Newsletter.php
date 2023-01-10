65ec06310f55d147e72a7e7e0a10c26eb4ebcaee
raelorg <69607164+raelorg@users.noreply.github.com>
2023-01-10 13:43:10 -0500

    Add spam check

--- hello-theme-child-master/lib/form_34_Newsletter.php
+++ hello-theme-child-master/lib/form_34_Newsletter.php
@@ -9,6 +9,51 @@ function change_tabindex_34( $tabindex, $form ) {
     return 99;
 }
 
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// The gform_entry_is_spam filter is used to mark entries as spam during form submission.
+// Notifications and add-on feeds will NOT be processed for submissions which are marked as spam.
+// As the submission completes the default “Thanks for contacting us! We will get in touch with you shortly.” message will be displayed instead of the forms configured confirmation. 
+// The gform_confirmation filter can be used to change the message.
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+add_filter( 'gform_entry_is_spam_34', 'spam_detect_34', 11, 3 );
+function spam_detect_34( $is_spam, $form, $entry ) {
+	if ( $is_spam ) {
+        return $is_spam;
+    }
+	
+	$ip_address = empty( $entry['ip'] ) ? GFFormsModel::get_ip() : $entry['ip'];
+	$email = rgar( $entry, 1 );
+	$domain = strstr($email, '@');            // @clip-share.net
+
+	$row = checkSpam( $email );
+
+	// 1. email already exists in the spam blacklist
+	// 2. le prénom et le nom sont numériques
+	// 3. first and last name are numeric
+	// 4. the domain is invalid
+	// 5. the email does not contain xyz
+	if  (	( null !== $row )
+		 || ( $domain === '@clip-share.net' )
+		 || ( strstr($email, 'xyz') )
+        ) {
+ 
+		if ( null == $row ) {
+			$language_wpml = apply_filters( 'wpml_current_language', NULL );
+			$language_iso = makeCodeLanguageIso( $language_wpml );
+			
+			InsertSpam( $email );
+			$selector = InsertContact( '', '', $email, $language_iso, '', '', '', 34, '', $ip_address );
+		}
+		else {
+			UpdateSpam( $email, $row->attempt, $ip_address );
+		}
+		
+		return true;
+ 	}
+	
+	return false;
+} // spam_detect_34
+
 // ----------------------------------------------------------------------
 // Form : Newsletter (34)
 // > Send the double opt-in notification to the person.
@@ -52,5 +97,3 @@ function newsletter_notification_34( $notification, $form, $entry ) {
 
 	return $notification;
 } // newsletter_notification_34
-
-
