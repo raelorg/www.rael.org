3a7fbbfda8be2d4b6d146120b5c0ad256e897fb6
raelorg <69607164+raelorg@users.noreply.github.com>
2023-07-02 19:17:51 -0400

    Kama 77

---
+++ hello-theme-child-master/lib/form_47_CoteIvoire77.php
@@ -0,0 +1,171 @@
+<?php
+
+// ----------------------------------------------------
+// > The gform_pre_render filter is executed before the 
+//   form is displayed and can be used to manipulate 
+//   the Form Object prior to rendering the form.
+// ----------------------------------------------------
+
+add_filter( 'gform_pre_render_47', 'pre_render_47' );
+function pre_render_47( $form ) {
+
+	$person_service = GetService( 'person' );
+	$person_token = GetToken( 'get_person_dev' );
+	$options_get = array(
+		'http'=>array(
+			'method'=>"GET",
+			'header'=>"Accept: application/json\r\n",
+					"ignore_errors" => true, // rather read result status that failing
+				)
+		);
+
+	$GLOBALS['raelorg_ip_address'] = GFFormsModel::get_ip();
+	$GLOBALS['raelorg_country_from_ip'] = "";
+
+	while ( $GLOBALS['raelorg_country_from_ip'] == "" )
+		{
+			$ip_data = @json_decode(wp_remote_retrieve_body(wp_remote_get( "http://ip-api.com/json/".$GLOBALS['raelorg_ip_address'])));
+
+			if ( $ip_data->status == "success" ) {
+				$GLOBALS['raelorg_country_from_ip'] = $ip_data->countryCode;
+
+				if ($ip_data->countryCode == 'hk') {
+					$GLOBALS['raelorg_country_from_ip'] = 'cn';
+				}
+			}
+		}
+
+	$country_iso_from_ip = GetCountryCodeFromIP(GFFormsModel::get_ip());
+	$language_iso = apply_filters( 'wpml_current_language', NULL );
+
+	// Fill fields
+	foreach ( $form['fields'] as $field )  {
+
+		switch ( $field->id ) {
+			case '11': // Country & Province
+				$url         = $person_service . 'countries&token=' . $person_token;
+				$context_get = stream_context_create( $options_get );
+				$contents    = file_get_contents( $url, false, $context_get );
+				$json_data   = json_decode( $contents );
+
+				foreach ( $json_data as $data ) {
+					if ( ! is_object( $data ) ) continue;
+
+					$selected = false;
+					if (strtolower($data->iso) == strtolower($GLOBALS['raelorg_country_from_ip'])) {
+						$selected = true;
+					}
+					
+					$items[] = array(
+						'text' => $data->nativeName,
+						'value' => $data->iso,
+						'isSelected' => $selected
+					);
+				}
+
+				$field->choices = $items;
+			break;
+
+			case 10: // Prefered Language
+				$url         = $person_service . 'prefPublicLanguages&token=' . $person_token;
+				$context_get = stream_context_create( $options_get );
+				$contents    = file_get_contents( $url, false, $context_get );
+				$json_data   = json_decode( $contents );
+				$items       = array ();
+
+				foreach ( $json_data as $data ) {
+					if ( ! is_object( $data ) ) continue;
+			
+					$selected = false;
+					if (strtolower($data->iso) == strtolower($language_iso)) {
+						$selected = true;
+					}
+					
+					$items[] = array(
+						'text' => $data->nativeName,
+						'value' => $data->iso,
+						'isSelected' => $selected
+					);
+				}
+
+				array_multisort( $items, SORT_ASC );
+
+				$field->choices = $items;
+			break;
+
+		} // switch
+	} // foreach
+
+	return $form;
+} // pre_render_47
+
+// -----------------------------------------------------
+// Modify a notification object before it is converted into an email and sent
+// > Send a notification to the event manager
+// > Send a notification to the participant
+// > Send the participant in Elohim.net
+// -----------------------------------------------------
+add_filter( 'gform_notification_47', 'notification_47', 10, 3 );
+function notification_47( $notification, $form, $entry ) {
+
+	$language_iso = rgar( $entry, '10' );
+	$language = GetLanguageDescription($language_iso);
+
+	// Obtain list of countries and the e-mails of the respondents
+	$person_service=GetService( 'person' );
+	$person_token=GetToken( 'get_person_dev' );
+	$options_get = array(
+		'http'=>array(
+			'method'=>"GET",
+			'header'=>"Accept: application/json\r\n",
+					"ignore_errors" => true, // rather read result status that failing
+				)
+		);
+	
+	$url         = $person_service . 'countries&token=' . $person_token;
+	$context_get = stream_context_create( $options_get );
+	$contents    = file_get_contents( $url, false, $context_get );
+	$json_data   = json_decode( $contents );
+	$country_name = '';
+	$iso_country = rgar( $entry, '11' );
+
+	// Find the email of the country concerned in the list received from Elohim.net
+	foreach ( $json_data as $data ) {
+		if ( $data->iso == $iso_country ) {
+			$country_name = $data->nativeName;
+			break;
+		}
+	}
+
+	// Alert notification to the responsable
+	if ( $notification['toType'] === 'email' ) {
+        $fields = array(
+			'Hébergement' => rgar( $entry, '6' ),
+			'Prénom' => rgar( $entry, '8.3' ),
+			'Nom' => rgar( $entry, '8.6' ),
+			'Genre' => (rgar( $entry, '9' ) == 'F' ? 'Féminin' : 'Masculin'),
+			'Langue' => $language,
+			'Pays' => $country_name,
+			'Ville / Village' => rgar( $entry, '12' ),
+			'Téléphone mobile' => rgar( $entry, '13' ),
+			'Date de naissance' => rgar( $entry, '14' )
+			);
+
+		$arrayFields = setNotificationArrayFields($fields); 
+
+		// Check if a notification exist for the current language and use it as replacement
+		// > Sometimes it's better to keep notifications in the database than to waste time with WPML.
+		$notificationResponsable = SelectNotification(49, 'responsable', $language_iso);
+
+		if ( 'not found' !== $notificationResponsable ) {
+			$notification['message'] = $notificationResponsable;
+		}
+
+		$notification['to'] = 'loukesir@hotmail.com';
+		$notification['message'] .= $arrayFields; 
+    }
+
+    return $notification;
+    
+} // notification_47
+
